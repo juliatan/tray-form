f46dabf87c3ce09aa5be8a0b21650960875ef9aa
Julia Tan <juliajtan@gmail.com>
2020-10-11 14:20:02 +0100

    Clean up Button test

--- src/components/UI/Button/Button.test.js
+++ src/components/UI/Button/Button.test.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Button from './Button';
 
 describe('Button component', () => {
-  afterEach(cleanup);
-
   it('should render', () => {
     const { asFragment } = render(<Button>Submit</Button>);
     expect(asFragment(<Button>Submit</Button>)).toMatchSnapshot();
